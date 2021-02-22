eb3a42f6eb3360a6dc5d16e93b6e28e3f8c82994
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-02-22 11:53:44 -0300

    Usando Singleton

--- src/main/java/br/com/alura/job/AgendamentoJob.java
+++ src/main/java/br/com/alura/job/AgendamentoJob.java
@@ -3,20 +3,20 @@ package br.com.alura.job;
 import java.util.List;
 
 import javax.ejb.Schedule;
-import javax.ejb.Stateless;
+import javax.ejb.Singleton;
 import javax.inject.Inject;
 
 import br.com.alura.entidade.Agendamento;
 import br.com.alura.servico.AgendamentoServico;
 
-@Stateless
+@Singleton
 public class AgendamentoJob {
 
 	@Inject
 	private AgendamentoServico agendamentoServico;
 	
 	@Schedule(hour = "*", minute = "*", second = "*/10")
-	public void enviarEmail() {
+	public synchronized void enviarEmail() {
 		List<Agendamento> listarPorNaoAgendado = agendamentoServico.listarPorNaoAgendado();
 		listarPorNaoAgendado.forEach(emailNaoAgendado -> {
 			agendamentoServico.enviar(emailNaoAgendado);
