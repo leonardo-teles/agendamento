5ff48fda86318613be78c130589b2ba8552007b0
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-02-22 14:04:19 -0300

    Criando Producer

--- src/main/java/br/com/alura/entidade/Agendamento.java
+++ src/main/java/br/com/alura/entidade/Agendamento.java
@@ -1,5 +1,7 @@
 package br.com.alura.entidade;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,7 +10,8 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "agendamento")
-public class Agendamento {
+public class Agendamento implements Serializable {
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/br/com/alura/job/AgendamentoJob.java
+++ src/main/java/br/com/alura/job/AgendamentoJob.java
@@ -2,9 +2,13 @@ package br.com.alura.job;
 
 import java.util.List;
 
+import javax.annotation.Resource;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.inject.Inject;
+import javax.jms.JMSConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.Queue;
 
 import br.com.alura.entidade.Agendamento;
 import br.com.alura.servico.AgendamentoServico;
@@ -15,11 +19,18 @@ public class AgendamentoJob {
 	@Inject
 	private AgendamentoServico agendamentoServico;
 	
+	@Inject
+	@JMSConnectionFactory("java:jboss/DefaultJMSConnectionFactory")
+	private JMSContext context;
+	
+	@Resource(mappedName = "java:/jms/queue/EmailQueue")
+	private Queue queue;
+	
 	@Schedule(hour = "*", minute = "*", second = "*/10")
 	public synchronized void enviarEmail() {
 		List<Agendamento> listarPorNaoAgendado = agendamentoServico.listarPorNaoAgendado();
 		listarPorNaoAgendado.forEach(emailNaoAgendado -> {
-			agendamentoServico.enviar(emailNaoAgendado);
+			context.createProducer().send(queue, emailNaoAgendado);
 			agendamentoServico.alterar(emailNaoAgendado);
 		});
 	}
