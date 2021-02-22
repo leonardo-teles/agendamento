ad3f9cdb79a20f5b8b24efd1277a76fc090a7567
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-02-21 22:37:00 -0300

    Trabalhando com Banco de Dados Usando JPA

---
+++ src/main/java/br/com/alura/dao/AgendamentoDAO.java
@@ -0,0 +1,20 @@
+package br.com.alura.dao;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.com.alura.entidade.Agendamento;
+
+@Stateless
+public class AgendamentoDAO {
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	public List<Agendamento> listar() {
+		return em.createQuery("SELECT a FROM Agendamento a", Agendamento.class).getResultList();
+	}
+}
--- src/main/java/br/com/alura/servico/AgendamentoEmailServico.java
+++ src/main/java/br/com/alura/servico/AgendamentoEmailServico.java
@@ -3,11 +3,18 @@ package br.com.alura.servico;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import br.com.alura.dao.AgendamentoDAO;
+import br.com.alura.entidade.Agendamento;
 
 @Stateless
 public class AgendamentoEmailServico {
+	
+	@Inject
+	private AgendamentoDAO dao; 
 
-	public List<String> listar() {
-		return List.of("leonardo.teles.almeida@gmail.com");
+	public List<Agendamento> listar() {
+		return dao.listar();
 	}
 }
