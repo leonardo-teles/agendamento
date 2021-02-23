35fb4554bdd595fa5a088906595d995be6121c1c
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-02-22 21:49:25 -0300

    Trabalhando com Transações BMT

--- src/main/java/br/com/alura/dao/AgendamentoDAO.java
+++ src/main/java/br/com/alura/dao/AgendamentoDAO.java
@@ -3,16 +3,24 @@ package br.com.alura.dao;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 
 import br.com.alura.entidade.Agendamento;
 
 @Stateless
+@TransactionManagement(TransactionManagementType.BEAN)
 public class AgendamentoDAO {
 
 	@PersistenceContext
 	private EntityManager em;
+	
+	@Inject
+	private UserTransaction userTransaction;
 	
 	public List<Agendamento> listar() {
 		return em.createQuery("SELECT a FROM Agendamento a", Agendamento.class).getResultList();
@@ -27,7 +35,13 @@ public class AgendamentoDAO {
 	}
 	
 	public void alterar(Agendamento agendamento) {
-		em.merge(agendamento);
+		try {
+			userTransaction.begin();
+			em.merge(agendamento);
+			userTransaction.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
 	}
 
 }
